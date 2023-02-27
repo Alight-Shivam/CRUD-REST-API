56683aecb79ab59afc715d1b5e0c854bb742d74e
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-02-27 19:34:25 +0530

    CRUD REST API

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ mongoose.connect(url, {useNewUrlParser:true})
 
 const con = mongoose.connection
 
-con.on('open', function(){
+con.on('open',() =>{
     console.log('connected...')
+})
+
+const alienRouter = require('./routes/aliens')
+app.use('/aliens', alienRouter)
+
+app.listen(9000, function(){
+    console.log('Server started')
 })
